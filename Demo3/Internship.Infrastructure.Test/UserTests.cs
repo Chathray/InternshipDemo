b973f5429b85157ad7e4e6eb945588027bfed219
Chathray <48251215+Chathray@users.noreply.github.com>
2021-04-12 17:43:08 +0700

    Create UserTests.cs

---
+++ Demo3/Internship.Infrastructure.Test/UserTests.cs
@@ -0,0 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace Internship.Infrastructure.Test
+{
+    public class UserTests
+    {
+        private readonly UserRespository _userRespository;
+        private readonly DataContext _memContext;
+
+        public UserTests()
+        {
+            var dbOptions = new DbContextOptionsBuilder<DataContext>()
+                .UseInMemoryDatabase(databaseName: "tmainternship")
+                .Options;
+
+            _memContext = new DataContext(dbOptions);
+            _userRespository = new UserRespository(_memContext);
+        }
+
+        [Fact]
+        public void Get_Existing_User()
+        {
+            var existingUser = Builders.GetUserDefault();
+
+            _memContext.Users.Add(existingUser);
+            _memContext.SaveChanges();
+
+            var testWithId = _userRespository.GetById(existingUser.UserId);
+            Assert.Equal(existingUser.UserId, testWithId.UserId);
+
+            var testWithPassword = _userRespository.GetUser(existingUser.Email, "zazaza");
+            Assert.Equal(existingUser.UserId, testWithPassword.UserId);
+        }
+    }
+}
